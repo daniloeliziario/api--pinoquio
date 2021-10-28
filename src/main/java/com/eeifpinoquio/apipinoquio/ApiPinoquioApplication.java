ecf681b7752a9b74793b4502003cb60b587e18c4
Danilo <danilo10dm@gmail.com>
2021-10-27 22:06:42 -0300

    Adicionando as configuracoes do spring

---
+++ src/main/java/com/eeifpinoquio/apipinoquio/ApiPinoquioApplication.java
@@ -0,0 +1,13 @@
+package com.eeifpinoquio.apipinoquio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPinoquioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPinoquioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eeifpinoquio/apipinoquio/ApiPinoquioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eeifpinoquio.apipinoquio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPinoquioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
