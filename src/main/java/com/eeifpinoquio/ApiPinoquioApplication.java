acfb1a71cfa3dc7f672a6d8fa90fc031247c2ef0
Danilo <danilo10dm@gmail.com>
2021-10-27 22:26:42 -0300

    Ajustando o pom e o renomeando o pacote principal

--- src/main/java/com/eeifpinoquio/ApiPinoquioApplication.java
+++ src/main/java/com/eeifpinoquio/ApiPinoquioApplication.java
@@ -1,4 +1,4 @@
-package com.eeifpinoquio.apipinoquio;
+package com.eeifpinoquio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
