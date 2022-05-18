daaba5684831fcd27ce55d82a7ffc02d53a36e93
Danilo <danilo10dm@gmail.com>
2022-05-18 00:12:07 -0300

    Metodo de calcular media

--- src/main/java/com/eeifpinoquio/domain/service/BimestreService.java
+++ src/main/java/com/eeifpinoquio/domain/service/BimestreService.java
@@ -1,5 +1,9 @@
 package com.eeifpinoquio.domain.service;
 
+import static org.apache.commons.lang3.ObjectUtils.allNotNull;
+import static org.apache.commons.lang3.ObjectUtils.isNotEmpty;
+import static org.apache.commons.lang3.math.NumberUtils.min;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -25,7 +29,8 @@ public class BimestreService {
 		bimestreAtual.setSegundaNota(bimestreAtualizado.getSegundaNota());
 		bimestreAtual.setTerceiraNota(bimestreAtualizado.getTerceiraNota());
 		bimestreAtual.setRecuperacao(bimestreAtualizado.getRecuperacao());
-		bimestreAtual.setFalta(bimestreAtualizado.getFalta());
+		bimestreAtual.setFalta(bimestreAtualizado.getFalta());		
+		bimestreAtual.setMedia(calcularMedia(bimestreAtual.getPrimeiraNota(), bimestreAtual.getSegundaNota(), bimestreAtual.getTerceiraNota(), bimestreAtual.getRecuperacao()));
 		
 		return boletimRepository.save(bimestreAtual);
 	}
@@ -33,6 +38,28 @@ public class BimestreService {
 	public Bimestre buscarOuFalhar(Long idBoletim, Long idDisciplina, Long idBimestre) {
 		return boletimRepository.findBimestreById(idBoletim, idDisciplina, idBimestre)
 				.orElseThrow(() -> new RecursoNaoEncontradoException(RECURSO_BIMESTRE, idBimestre));
+	}
+	
+	private Double calcularMedia(Double primeiraNota, Double segundaNota, Double terceiraNota, Double recuperacao) {
+		
+		Double media = null;
+		
+		if(allNotNull(primeiraNota, segundaNota, terceiraNota)) {
+			
+			double menor = min(primeiraNota, segundaNota, terceiraNota);
+			
+			if(isNotEmpty(recuperacao) && recuperacao > menor) {				
+				
+				if(primeiraNota.equals(menor)) {
+					return (recuperacao + segundaNota + terceiraNota) / 3;
+				} else if(segundaNota.equals(menor)) {
+					return (primeiraNota + recuperacao + terceiraNota) / 3;
+				}
+				return (primeiraNota + segundaNota + recuperacao) / 3;
+			}
+			return (primeiraNota + segundaNota + terceiraNota) / 3;
+		}
+		return media;
 	}
 
 }
